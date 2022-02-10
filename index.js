5b6f32182bbf64fb869611bbe481056de21f367f
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-02-10 13:36:28 +0100

    arreglo rutas

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('./utils/dbmongo');
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req,res) =>{
+app.get('/', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
